d020f474e7862cd1cac0b3dd460e73245fd31c0f
Ivan Dyedov <idyedov@50onred.com>
2013-03-21 13:47:53 -0400

    add option to pass graphite IP to diamond config using attributes

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,12 +10,27 @@ service "diamond" do
   action [ :enable ]
 end
 
-cookbook_file "/etc/diamond/diamond.conf" do
-  source "diamond.conf"
+if node['diamond']['graphite_server_role'].nil?
+  graphite_ip = node['diamond']['graphite_server']
+else
+  graphite_nodes = search(:node, "role:%s" % [node['diamond']['graphite_server_role']])
+  if graphite_nodes.empty?
+    Chef::Log.warn("No nodes returned from search")
+    graphite_ip = node['diamond']['graphite_server']
+  else
+    graphite_ip = graphite_nodes[0]["ipaddress"]
+  end
+end
+
+template "/etc/diamond/diamond.conf" do
+  source "diamond.conf.erb"
   owner "root"
   group "root"
   mode "0644"
   notifies :restart, resources(:service => "diamond")
+  variables(
+    :graphite_ip => graphite_ip
+  )
 end
 
 #install basic collector configs
