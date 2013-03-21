f6bfe86755759ad9693b7ecfda5698e3584c46c6
Ivan Dyedov <idyedov@50onred.com>
2013-03-21 12:59:50 -0400

    add some missing default attributes for diskspace and nginx collectors

--- attributes/diskspace.rb
+++ attributes/diskspace.rb
@@ -1,1 +1,3 @@
-
+default[:diamond][:collectors][:DiskSpaceCollector][:filesystems] = 'ext2, ext3, ext4, xfs, glusterfs, nfs, ntfs, hfs, fat32, fat16'
+default[:diamond][:collectors][:DiskSpaceCollector][:exclude_filters] = '/export/home'
+default[:diamond][:collectors][:DiskSpaceCollector][:byte_unit] = 'byte'
--- attributes/nginx.rb
+++ attributes/nginx.rb
@@ -1,2 +1,4 @@
 default[:diamond][:collectors][:NginxCollector][:path] = "nginx"
+default[:diamond][:collectors][:NginxCollector][:req_path] = "/nginx_status"
 default[:diamond][:collectors][:NginxCollector][:req_host] = "localhost"
+default[:diamond][:collectors][:NginxCollector][:req_port] = "8080"
