49d0af37f27dba5c3a61b94a198094a829bb318c
Joshua Buysse <buysse@umn.edu>
2013-10-29 15:19:44 -0500

    Added 'name' field to metadata to make Berkshelf happy.

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,8 @@ license          "All rights reserved"
 description      "Installs/Configures diamond"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.17"
+name             "diamond"
 
 supports         'ubuntu'
 supports         'centos'
+
