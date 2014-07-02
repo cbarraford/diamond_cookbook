8241b70820a0a463383819782121b521ea2408c8
Chad Barraford <cbarraford@gmail.com>
2014-07-02 15:20:11 -0400

    use master as default

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,7 +6,7 @@ default["diamond"]["interval"] = "300"
 default["diamond"]["handlers"] = "diamond.handler.graphite.GraphiteHandler, diamond.handler.archive.ArchiveHandler"
 default["diamond"]["source_repository"] = "git://github.com/BrightcoveOS/Diamond.git"
 default["diamond"]["source_path"] = "/usr/local/share/diamond_src"
-default["diamond"]["source_reference"] = 'HEAD'
+default["diamond"]["source_reference"] = 'master'
 default['diamond']['add_collectors'] = ['cpu', 'diskspace', 'diskusage', 'loadavg', 'memory', 'network', 'vmstat', 'tcp']
 case node["platform_family"]
   when "debian"
